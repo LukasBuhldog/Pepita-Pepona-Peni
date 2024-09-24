1bfcec73d411e31055bfe4a0712f4766bcb01ac8
LukasBuhldog <klooby-3321@hotmail.com>
2024-09-24 11:49:29 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+
+let gramsToEat = undefined;
+let kmsToFly = undefined;
+
+const pepita = reactice({
+  energy: 0,
+})
+
+function displayGrams(){
+  console.log(gramsToEat);
+}
+
+function displayKms(){
+  console.log(kmsToFly)
+}
+
+function eat(){
+pepita.energy += gramsToEat * 4;
+console.log(pepita.energy)
+}
+
+function fly(){
+  pepita.energy -= kmsToFly * 2 + 10;
+  console.log(pepita.energy)
+}
+</script>
+
+
+<template>
+<h2 id=pepitaEnergy> Pepita's Energy: {{ pepita.energy }} </h2>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8pmzkiZrt6qzwXx0NY_-v6bCsEdF1sJ4pew&s"></img>
+
+<input type="number" v-model="gramsToEat" placeholder="Grams">
+<button @click='displayGrams'>Eat</button>
+
+<input type="number" v-model="kmsToFly" placeholder="Kilometers">
+<button @click='displayKms'>Fly</button>
+</template>
+
+
+
+<style scoped>
+</style>
